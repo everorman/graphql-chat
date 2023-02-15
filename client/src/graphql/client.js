1095c9355d5cdc66c1d646b3543020b8d3666b9f
Evert Ortiz <evert.ortiz.m@gmail.com>
2023-02-15 16:50:24 -0500

    Add auth token in client

--- client/src/graphql/client.js
+++ client/src/graphql/client.js
@@ -3,12 +3,14 @@ import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
 import { getMainDefinition } from '@apollo/client/utilities';
 import { createClient as createWsClient } from 'graphql-ws';
 import { Kind, OperationTypeNode } from 'graphql';
+import { getAccessToken } from '../auth';
 
 const httpLink = new HttpLink({
   uri: 'http://localhost:9090/graphql'
 });
 const wsLink = new GraphQLWsLink(createWsClient({
-  url: 'ws://localhost:9090/graphql'
+  url: 'ws://localhost:9090/graphql',
+  connectionParams: () => ({ accessToken: getAccessToken() })
 }));
 
 function isSubscription({ query }) {
